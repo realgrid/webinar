de8eefa91e4b80857530a453af9942eb50d50f1d
KIMSANGYEOB <sykim10070@gmail.com>
2026-08-25 18:00:04 +0900

    Feat: 프로젝트 생성

---
+++ 2026-09/realreport/src/license.ts
@@ -0,0 +1,3 @@
+// 이 라이센스는 2026-12-31까지 localhost, 127.0.0.1 에서 사용 가능한 라이센스입니다.
+export const REALREPORT_LICENSE =
+  'upVcPE+wPOkcfqywe+clVN+UVTCvO3is+83EYTz6U/ufti7oHFstjC3QMBxzmMIozyXlaBz8ES1IVaBfqOuLyTQR9hbhIVddO6pBj8PqGa3iYFY7cKro17JqzYC94+DMm+gcqnmKqNt8a7Wytk9FrL9Uxls4sDexdTY3AepZ5QxSJ1omHLHJlF9t0XxSSQ+pXDKIuJghkwM=';
